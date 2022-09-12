636ff0e91ea6948c791755be865617f88428993e
Pablo Rodrigues <pablorodrigues606@gmail.com>
2022-09-12 14:48:02 -0700

    html

---
+++ script.js
@@ -0,0 +1,71 @@
+//Concatenação de Strings
+/*var nome = "Pablo Rodrigues"
+var idade = "25"
+
+console.log("Bem vindo, " + nome)
+console.log("Você tem " + idade  + " anos")
+*/
+
+
+
+//Substituindo conteúdo da string com replace
+/*var frase = "Argentina é o melhor time do mundo"
+
+console.log(frase.replace("Argentina", "Brasil"))
+*/
+
+
+//Array
+/*var lista =  ["pera", "uva", "banana"];
+lista.push("morango");
+lista.pop("morango");
+console.log(lista)
+console.log(lista.toString())
+console.log(lista.join(" | "))
+*/
+
+//Dicionário
+/*
+var fruta = {nome: "uva", cor: "roxo"};
+console.log(fruta)
+console.log(fruta.cor)
+console.log(fruta.nome)
+*/
+
+//Lista de dicionários
+/*
+var frutas = [{nome: "uva", cor: "roxo"}, {nome: "banana", cor: "amarela"}, {nome: "morango", cor: "vermelho"}];
+//console.log(frutas)
+console.log(frutas)
+*/
+
+//Condicionais
+/*
+var idade = prompt("Qual a sua idade?")
+if (idade >= 18) {
+    console.log("Maior de idade!")
+}  else {
+    console.log("Menor de idade!")
+}
+*/
+
+//Laços de repitação
+//while
+/*
+var count = 0;
+while(count <= 5) {
+    console.log(count)
+    count = count + 1;
+}
+*/
+
+//for 
+/*var count;
+for(count = 0; count <= 5; count++) {
+    console.log(count);
+};
+*/
+
+//Data
+var d = new Date();
+console.log(d.getHours());
